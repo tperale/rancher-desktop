4d179e873394cd4c6bc980ad3a4726f6dd2f7595
Thomas Perale <thomas.perale@mind.be>
2023-03-28 15:37:22 +0200

    Improve start/stop service for WSL backend
    Signed-off-by: Thomas Perale <thomas.perale@mind.be>

--- pkg/rancher-desktop/backend/wsl.ts
+++ pkg/rancher-desktop/backend/wsl.ts
@@ -1126,15 +1126,34 @@ export default class WSLBackend extends events.EventEmitter implements VMBackend
   }
 
   /**
+   * Execute a command on a given OpenRC service.
+   *
+   * @param service The name of the OpenRC service to execute.
+   * @param action The name of the OpenRC service action to execute.
+   * @param argument Argument to pass to `wsl-service` (`--ifnotstart`, `--ifstarted`)
+   */
+  async execService(service: string, action: string, argument = '') {
+    await this.execCommand('/usr/local/bin/wsl-service', argument, service, action);
+  }
+
+  /**
    * Start the given OpenRC service.  This should only happen after
    * provisioning, to ensure that provisioning can modify any configuration.
    *
    * @param service The name of the OpenRC service to execute.
    */
   async startService(service: string) {
-    // Run rc-update as we have dynamic dependencies.
     await this.execCommand('/sbin/rc-update', '--update');
-    await this.execCommand('/usr/local/bin/wsl-service', service, 'start');
+    await this.execService(service, 'start', '--ifnotstarted');
+  }
+
+  /**
+   * Stop the given OpenRC service.
+   *
+   * @param service The name of the OpenRC service to stop.
+   */
+  async stopService(service: string) {
+    await this.execService(service, 'stop', '--ifstarted');
   }
 
   /**
@@ -1363,7 +1382,7 @@ export default class WSLBackend extends events.EventEmitter implements VMBackend
         }
         if (config.containerEngine.name === ContainerEngine.CONTAINERD) {
           await this.progressTracker.action('Starting buildkit', 0,
-            this.execCommand('/usr/local/bin/wsl-service', '--ifnotstarted', 'buildkitd', 'start'));
+            this.startService('buildkitd'));
         }
 
         switch (config.containerEngine.name) {
@@ -1465,7 +1484,7 @@ export default class WSLBackend extends events.EventEmitter implements VMBackend
 
         // Stop the service if it's already running for some reason.
         // This should never be the case (because we tore down init).
-        await this.execCommand('/usr/local/bin/wsl-service', '--ifstarted', 'local', 'stop');
+        await this.stopService('local');
 
         // Clobber /etc/local.d and replace it with a symlink to our desired
         // path.  This is needed as /etc/init.d/local does not support
@@ -1481,7 +1500,7 @@ export default class WSLBackend extends events.EventEmitter implements VMBackend
           '-print', '-exec', 'chmod', 'a+x', '{}', ';');
 
         // Run the script.
-        await this.execCommand('/usr/local/bin/wsl-service', 'local', 'start');
+        await this.startService('local');
       })(),
     ]);
   }
@@ -1503,14 +1522,14 @@ export default class WSLBackend extends events.EventEmitter implements VMBackend
 
       await this.progressTracker.action('Shutting Down...', 10, async() => {
         if (await this.isDistroRegistered({ runningOnly: true })) {
-          await this.execCommand('/usr/local/bin/wsl-service', '--ifstarted', 'k3s', 'stop');
-          await this.execCommand('/usr/local/bin/wsl-service', '--ifstarted', 'docker', 'stop');
-          await this.execCommand('/usr/local/bin/wsl-service', '--ifstarted', 'containerd', 'stop');
-          await this.execCommand('/usr/local/bin/wsl-service', '--ifstarted', 'openresty', 'stop');
-          await this.execCommand('/usr/local/bin/wsl-service', '--ifstarted', 'rancher-desktop-guestagent', 'stop');
-          await this.execCommand('/usr/local/bin/wsl-service', '--ifstarted', 'buildkitd', 'stop');
+          await this.stopService('k3s');
+          await this.stopService('docker');
+          await this.stopService('containerd');
+          await this.stopService('openresty');
+          await this.stopService('rancher-desktop-guestagent');
+          await this.stopService('buildkitd');
           try {
-            await this.execCommand('/usr/local/bin/wsl-service', '--ifstarted', 'local', 'stop');
+            await this.stopService('local');
           } catch (ex) {
             // Do not allow errors here to prevent us from stopping.
             console.error('Failed to run user provisioning scripts on stopping:', ex);
